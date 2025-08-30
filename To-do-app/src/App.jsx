48220f8359847721db3de97737bb0ba432a66360
Sahibzada Hasanat Ahmad <sahibzadahasanatahmad@gmail.com>
2025-08-30 14:02:13 +0500

    Todo list added

---
+++ To-do-app/src/App.jsx
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <center class="todo-container">
+      <h1>TODO App</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter todo here" />
+          </div>
+          <div class="col-4">
+            <input type="date" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">
+              Success
+            </button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-6">Buy milk</div>
+          <div class="col-4">10/10/2004</div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
+}
+
+export default App;
